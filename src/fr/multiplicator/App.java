89ceb838e04a15cc55f6df57d4a64cef0facd213
impskil <mikael.guez@gmail.com>
2022-05-13 18:50:06 +0200

    mise à jour

--- src/fr/multiplicator/App.java
+++ src/fr/multiplicator/App.java
@@ -11,7 +11,7 @@ public class App {
     private JPanel panelMain;
     private JTextField textFieldReponse;
     private JLabel labelCalcul;
-    private JButton buttonGenerer;
+
     private JLabel labelResult;
     private JSlider sliderDifficult;
     private JLabel labelDifficult;
@@ -24,55 +24,46 @@ public class App {
     public App() {
         labelDifficult.setText(Integer.toString(sliderDifficult.getValue()));
         methodeNvCalcul();
-        button_msg.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (textFieldReponse.getText().matches("-?\\d+") && textFieldReponse.getText().length() != 0) {
-                    //Saisie correcte
-                    int result = Integer.parseInt(labelResult.getText());
-                    int verifResultat = Integer.parseInt(textFieldReponse.getText());
-                    int resultatAVerifier = Integer.parseInt(labelResult.getText());
-                    if (verifResultat == resultatAVerifier) {
-                        //JOptionPane.showConfirmDialog(null, "Bien joué !");
-                        labelMessage.setText("Bien joué !");
-                        int Score = Integer.parseInt(labelScore.getText()) + 1 ;
-                        int Total = Integer.parseInt(labelTotal.getText()) + 1 ;
-                        labelScore.setText(Integer.toString(Score));
-                        labelTotal.setText(Integer.toString(Total));
+        button_msg.addActionListener(e -> {
+            if (textFieldReponse.getText().matches("-?\\d+") && textFieldReponse.getText().length() != 0) {
+                //Saisie correcte
+                int verifResultat = Integer.parseInt(textFieldReponse.getText());
+                int resultatAVerifier = Integer.parseInt(labelResult.getText());
+                if (verifResultat == resultatAVerifier) {
+                    //JOptionPane.showConfirmDialog(null, "Bien joué !");
+                    labelMessage.setText("Bien joué !");
+                    int Score = Integer.parseInt(labelScore.getText()) + 1 ;
+                    int Total = Integer.parseInt(labelTotal.getText()) + 1 ;
+                    labelScore.setText(Integer.toString(Score));
+                    labelTotal.setText(Integer.toString(Total));
 
-                    } else {
-                        //JOptionPane.showConfirmDialog(null, "C'est raté");
-                        labelMessage.setText("C'est raté");
-                        int Total = Integer.parseInt(labelTotal.getText()) + 1 ;
-                        labelTotal.setText(Integer.toString(Total));
-                    }
                 } else {
-                    //saisie incorrecte
-                    //JOptionPane.showConfirmDialog(null, "Saisir incorrecte");
-                    labelMessage.setText("Saisir incorrecte");
+                    //JOptionPane.showConfirmDialog(null, "C'est raté");
+                    labelMessage.setText("C'est raté");
+                    int Total = Integer.parseInt(labelTotal.getText()) + 1 ;
+                    labelTotal.setText(Integer.toString(Total));
                 }
-                float Ratio = Float.parseFloat(labelScore.getText()) / Float.parseFloat(labelTotal.getText()) * 100;
-                labelRatio.setText(Float.toString(Math.round(Ratio)) + "%");
-                textFieldReponse.setText(null);
                 methodeNvCalcul();
             }
+            else {
+                //saisie incorrecte
+                //JOptionPane.showConfirmDialog(null, "Saisir incorrecte");
+                labelMessage.setText("Saisie incorrecte");
+            }
+            float Ratio = Float.parseFloat(labelScore.getText()) / Float.parseFloat(labelTotal.getText()) * 100;
+            labelRatio.setText(Float.toString(Math.round(Ratio)) + "%");
+            textFieldReponse.setText(null);
+
         });
 
 
-        sliderDifficult.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                labelDifficult.setText(Integer.toString(sliderDifficult.getValue()));
-            }
-        });
+        sliderDifficult.addChangeListener(e -> labelDifficult.setText(Integer.toString(sliderDifficult.getValue())));
 
-        resetButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                labelScore.setText("0");
-                labelTotal.setText("0");
-                methodeNvCalcul();
-            }
+        resetButton.addActionListener(e -> {
+            labelScore.setText("0");
+            labelTotal.setText("0");
+            methodeNvCalcul();
+            labelRatio.setText("--%");
         });
     }
 
@@ -92,7 +83,7 @@ public class App {
         int a = (int) (Math.random() * (aMax - aMin + 1) + aMin);
         int b = (int) (Math.random() * (bMax - bMin + 1) + bMin);
         int result = a * b;
-        labelCalcul.setText(Integer.toString(a) + " x " + Integer.toString(b));
+        labelCalcul.setText(a + " x " + b);
         labelResult.setText(Integer.toString(result));
     }
 }
